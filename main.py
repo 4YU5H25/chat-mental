cf314b9f214b76b8ef2df53f0414b4aaa23e2b23
Jjateen Gundesha <116161686+Jjateen@users.noreply.github.com>
2024-04-06 19:27:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -35,5 +35,3 @@ async def predict(item: ScoringItem):
 @app.get('/')
 async def read_root():
     return "Welcome to Mental Wellness ChatBot!"
-
-    # uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
